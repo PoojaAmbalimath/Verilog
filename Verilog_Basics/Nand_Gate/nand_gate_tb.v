8a7292b757c137d87e6a9541b9c57fcc7cc37be5
Pooja Ambalimath <anitaambalimath@gmail.com>
2026-06-28 15:36:06 +0530

    Create nand_gate_tb.v

---
+++ Verilog_Basics/Nand_Gate/nand_gate_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module nand_gate_tb;
+
+reg A;
+reg B;
+wire Y;
+
+// Instantiate the NAND gate
+nand_gate uut (
+    .A(A),
+    .B(B),
+    .Y(Y)
+);
+
+initial begin
+    $display("A B | Y");
+    $monitor("%b %b | %b", A, B, Y);
+
+    A = 0; B = 0; #10;
+    A = 0; B = 1; #10;
+    A = 1; B = 0; #10;
+    A = 1; B = 1; #10;
+
+    $finish;
+end
+
+endmodule
